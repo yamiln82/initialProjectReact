e3b707b2f56d3cccf8a9505327271910a96cae66
yamiln82 <yamilnu82@gmail.com>
2024-05-20 22:39:41 -0500

    actividad 2 de la creacion de componentes

--- src/Component.js
+++ src/Component.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Component() {
+  
+
+  const [text, setText] = useState () 
+  const [updated, setUpdated] = useState () 
+
+  const textOnChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const buttonOnclick = () => {
+    setUpdated(text)
+  }
+
+  
+  
   return (
     <div>
-      My Component</div>
+      <input type = "text" value={text} onChange={textOnChange} />
+      <button onClick={buttonOnclick}>Actualizar </button>
+     <p>Texto input: {text}</p> 
+     <p>Texto Actualizado: {updated}</p>
+     
+      
+      </div>
   )
 }
